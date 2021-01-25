c3b5fb2a450d842bb123ee342722e3247e05eff7
Alexander Kosmehl <alexander_chrysanth.kosmehl@smail.th-koeln.de>
2021-01-25 21:21:47 +0100

    Fixed Indentation

--- Day1/Day1_Solution.js
+++ Day1/Day1_Solution.js
@@ -14,7 +14,7 @@ part_one: // Label is required to break out of all loops
         for (let j = 0; j < input.length; j++) {
             if (i === j) continue
             if (input[i] + input[j] === 2020) {
-                console.log(`Solution found at ${ input[i] } + ${ input[j] }! The solution is ${ input[i] * input[j] }`)
+                console.log(`Solution found at ${input[i]} + ${input[j]}! The solution is ${input[i] * input[j]}`)
                 break part_one
             }
         }
@@ -27,7 +27,7 @@ part_two:
             for (let k = 0; k < input.length; k++) {
                 if (i === j || i === k || j === k) continue
                 if (input[i] + input[j] + input[k] === 2020) {
-                    console.log(`Solution found at ${ input[i] } + ${ input[j] } + ${ input[k] }! The solution is ${ input[i] * input[j] * input[k] }`)
+                    console.log(`Solution found at ${input[i]} + ${input[j]} + ${input[k]}! The solution is ${input[i] * input[j] * input[k]}`)
                     break part_two
                 }
             }
