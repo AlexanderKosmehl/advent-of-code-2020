119ae0c61b941bfbe56a63ecad0c23c59d202540
Alexander Kosmehl <alexander_chrysanth.kosmehl@smail.th-koeln.de>
2021-01-25 21:01:35 +0100

    -Added solution for part 2 -Fixed loop breaks

--- Day1/Day1_Solution.js
+++ Day1/Day1_Solution.js
@@ -7,12 +7,27 @@ const fs = require("fs")
 const input = fs.readFileSync("Day1_Input.txt", "utf-8").split("\n").map(x => parseInt(x))
 
 // Part 1
+loop_breaker:
 for (let i = 0; i < input.length; i++) {
     for (let j = 0; j < input.length; j++) {
         if (i === j) continue
         if (input[i] + input[j] === 2020) {
-            console.log(`Solution found at ${input[i]} + ${input[j]}! The solution is ${input[i]*input[j]}`)
-            return
+            console.log(`Solution found at ${ input[i] } + ${ input[j] }! The solution is ${ input[i] * input[j] }`)
+            break loop_breaker
+        }
+    }
+}
+
+// Part 2
+loop_breaker:
+for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+        for (let k = 0; k < input.length; k++) {
+            if (i === j || i === k || j === k) continue
+            if (input[i] + input[j] + input[k] === 2020) {
+                console.log(`Solution found at ${ input[i] } + ${ input[j] } + ${ input[k] }! The solution is ${ input[i] * input[j] * input[k] }`)
+                break loop_breaker
+            }
         }
     }
 }
