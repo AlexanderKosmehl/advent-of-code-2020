63bcee9d6ce6e7936bbec263b8e10efe5204c2c0
Alexander Kosmehl <alexander_chrysanth.kosmehl@smail.th-koeln.de>
2021-01-26 22:52:45 +0100

    Initial solution code

--- Day3/Day3_Solution.js
+++ Day3/Day3_Solution.js
@@ -1,3 +1,12 @@
 /*
 https://adventofcode.com/2020/day/3
  */
+
+const fs = require("fs")
+
+let input = fs.readFileSync("Day3_Input.txt", "utf-8").split("\r\n")
+
+for (let line of input) {
+    console.log(line)
+}
+
