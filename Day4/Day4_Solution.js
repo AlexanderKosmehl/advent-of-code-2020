ba5a2344244e591313b3c0848fd264382e7d8c66
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-07-15 17:52:36 +0200

    Added part 2 solution

--- Day4/Day4_Solution.js
+++ Day4/Day4_Solution.js
@@ -44,6 +44,57 @@ function checkPassportString (passportString) {
   return true
 }
 
+function validatePassportString (passportString) {
+  // Birth Year       four digits; at least 1920 and at most 2002.
+  const birthYearMatch = passportString.match(/(?<=byr:)\d{4}(?!\w)/)
+  if (!birthYearMatch) return false
+  const birthYear = Number(birthYearMatch[0])
+  if (birthYear < 1920 || birthYear > 2002) return false
+
+  // Issue Year       four digits; at least 2010 and at most 2020.
+  const issueYearMatch = passportString.match(/(?<=iyr:)\d{4}(?!\w)/)
+  if (!issueYearMatch) return false
+  const issueYear = Number(issueYearMatch[0])
+  if (issueYear < 2010 || issueYear > 2020) return false
+
+  // Expiration Year  four digits; at least 2020 and at most 2030.
+  const expirationYearMatch = passportString.match(/(?<=eyr:)\d{4}(?!\w)/)
+  if (!expirationYearMatch) return false
+  const expirationYear = Number(expirationYearMatch[0])
+  if (expirationYear < 2020 || expirationYear > 2030) return false
+
+  // Height           a number followed by either cm or in:
+  //                        If cm, the number must be at least 150 and at most 193.
+  //                        If in, the number must be at least 59 and at most 76.
+  const heightMatch = passportString.match(/(?<=hgt:)(\d+)(cm|in)(?!\w)/)
+  if (!heightMatch) return false
+  const height = Number(heightMatch[1])
+  const heightUnit = heightMatch[2]
+  if (heightUnit === 'cm') {
+    if (height < 150 || height > 193) return false
+  } else if (heightUnit === 'in') {
+    if (height < 59 || height > 76) return false
+  } else return false
+
+  // Hair Color       a # followed by exactly six characters 0-9 or a-f.
+  const hairColorMatch = passportString.match(/(?<=hcl:)#[0-9a-f]{6}(?!\w)/)
+  if (!hairColorMatch) return false
+
+  // Eye Color        exactly one of: amb blu brn gry grn hzl oth.
+  const eyeColorMatch = passportString.match(/(?<=ecl:)(amb|blu|brn|gry|grn|hzl|oth)(?!\w)/)
+  if (!eyeColorMatch) return false
+
+  // Passport ID      a nine-digit number, including leading zeroes.
+  const passportIdMatch = passportString.match(/(?<=pid:)\d{9}(?!\w)/)
+  if (!passportIdMatch) return false
+
+  // Country ID       ignored, missing or not.
+  // Nothing to do here
+
+  // If no test failed the string is valid
+  return true
+}
+
 /*
     Solution Code
 */
@@ -52,3 +103,4 @@ function checkPassportString (passportString) {
 const inputLines = input.split('\r\n\r\n').map(line => line.replace(/\r\n/g, ' '))
 
 console.log('Solution to part 1:', inputLines.filter(checkPassportString).length)
+console.log('Solution to part 2:', inputLines.filter(validatePassportString).length)
