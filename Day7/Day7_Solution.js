4aecfd3e7e4921460e5ba2d19d445813db0d31bc
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-03 15:36:36 +0200

    Added initial solution file for day 7

---
+++ Day7/Day7_Solution.js
@@ -0,0 +1,12 @@
+/*
+    https://adventofcode.com/2020/day/6
+*/
+
+const path = require('path')
+const fs = require('fs')
+const input = fs.readFileSync(path.resolve(__dirname, 'Day7_Input.txt'), 'utf-8')
+const { _ } = require('./Day7_Helper')
+
+/*
+  Solution Code
+*/
